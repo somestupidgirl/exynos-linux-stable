ef667363accf5cd77bea90b53c5cc128af138ee5
Peter Zijlstra <peterz@infradead.org>
2016-10-04 16:29:45 +0200

    BACKPORT: sched/core: Add {EN,DE}QUEUE_NOCLOCK flags
    Currently {en,de}queue_task() do an unconditional update_rq_clock().
    However since we want to avoid duplicate updates, so that each
    rq->lock section appears atomic in time, we need to be able to skip
    these clock updates.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: Sunneva N. Mariu <sunnevanattsol@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -758,9 +758,12 @@ static void set_load_weight(struct task_struct *p)
 
 static inline void enqueue_task(struct rq *rq, struct task_struct *p, int flags)
 {
-	update_rq_clock(rq);
+	if (!(flags & ENQUEUE_NOCLOCK))
+		update_rq_clock(rq);
+
 	if (!(flags & ENQUEUE_RESTORE)) {
 		sched_info_queued(rq, p);
+
 		psi_enqueue(p, flags & ENQUEUE_WAKEUP);
 	}
 
@@ -771,9 +774,12 @@ static inline void enqueue_task(struct rq *rq, struct task_struct *p, int flags)
 
 static inline void dequeue_task(struct rq *rq, struct task_struct *p, int flags)
 {
-	update_rq_clock(rq);
+	if (!(flags & DEQUEUE_NOCLOCK))
+		update_rq_clock(rq);
+
 	if (!(flags & DEQUEUE_SAVE)) {
 		sched_info_dequeued(rq, p);
+
 		psi_dequeue(p, flags & DEQUEUE_SLEEP);
 	}
 
