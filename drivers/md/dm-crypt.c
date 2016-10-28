c958abd87a8df4f3ae5a8fc9c1ef51b94476e043
Christoph Hellwig <hch@lst.de>
2016-10-28 08:48:16 -0600

    block: better op and flags encoding
    Now that we don't need the common flags to overflow outside the range
    of a 32-bit type we can encode them the same way for both the bio and
    request fields.  This in addition allows us to place the operation
    first (and make some room for more ops while we're at it) and to
    stop having to shift around the operation values.
    
    In addition this allows passing around only one value in the block layer
    instead of two (and eventuall also in the file systems, but we can do
    that later) and thus clean up a lot of code.
    
    Last but not least this allows decreasing the size of the cmd_flags
    field in struct request to 32-bits.  Various functions passing this
    value could also be updated, but I'd like to avoid the churn for now.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Jens Axboe <axboe@fb.com>
    Signed-off-by: Sunneva N. Mariu <sunnevanattsol@gmail.com>

--- drivers/md/dm-crypt.c
+++ drivers/md/dm-crypt.c
@@ -1276,7 +1276,7 @@ static void clone_init(struct dm_crypt_io *io, struct bio *clone)
 	clone->bi_private = io;
 	clone->bi_end_io  = crypt_endio;
 	clone->bi_bdev    = cc->dev->bdev;
-	bio_set_op_attrs(clone, bio_op(io->base_bio), bio_flags(io->base_bio));
+	clone->bi_opf	  = io->base_bio->bi_opf;
 #ifdef CONFIG_JOURNAL_DATA_TAG
 	clone->bi_flags   |= io->base_bio->bi_flags & (1 << BIO_JOURNAL);
 #endif
