d3180347dc804ee155cd347a69af96a3c6467877
somestupidgirl <sunnevanattsol@gmail.com>
2025-01-24 14:58:08 +0000

    Build scripts: Major fixes and changes
    Build scripts should run straight out of the box now.
    
    To build the kernel run:
    
        $ ./build<device>.sh <stock/aosp> <ksu/noksu> <ext4/erofs>
    
    List of changes:
    
        - Add more coherent input arguments for KernelSU and EROFS to build scripts
    
        - Fix sed not working in some instances in variables.sh
    
        - Remove KernelSU from drivers/Kconfig and Makefile as it was causing non-KernelSU builds to fail
    
        - Add instructions to variables.sh for inserting KernelSU lines to drivers/Kconfig and Makefile when KernelSU is enabled
    
        - Make clean.sh clean up more KernelSU-related stuff
    
        - Automatically create output directories (../zip/kernel/... etc.) upon finishing builds
    
        - Make zip.sh create a flashable zip with Aroma installer after running build_kernel.sh

--- zip.sh
+++ zip.sh
@@ -1,6 +1,43 @@
 #!/bin/bash
 
-printf "Zipping\n"
-ZIP_DIR="/home/$USER/Android/Kernel/Zip"
-cd $ZIP_DIR
-./zip.sh
+. variables.sh
+
+printf "Creating flashable zip...\n"
+
+cp -R $KERN_DIR/Kernel-stock $KERN_DIR/Kernel
+
+# Copy kernel and dtb to boot image base
+cp $KERN_DIR/Kernel/crownlte/zImage $BOOT_DIR/split/kernel
+cp $KERN_DIR/Dtb/crownlte/dtb.img $BOOT_DIR/split/extra
+
+# Repack empty-boot.img
+cd $BOOT_DIR && repackimg
+
+# Move empty-boot.img to Kernel directory
+mv $BOOT_DIR/new-boot.img $KERN_DIR/empty-boot.img
+
+# Compress Kernel.tar.zst
+cd $ZIP_DIR && tar --zstd -cf Kernel.tar.zst Kernel
+
+# Move Kernel.tar.zst to Aroma directory
+mv $ZIP_DIR/Kernel.tar.zst $ZIP_DIR/Aroma
+
+# Zip the Aroma installer directory
+cd $ZIP_DIR && zip -r -o Aroma.zip $ZIP_DIR/Aroma/*
+
+# Create Output directory
+if [ ! -d $OUT_DIR ]; then
+  mkdir -p $OUT_DIR
+fi
+
+# Move Aroma.zip to Output directory
+mv Aroma.zip $OUT_DIR/Aroma.zip
+
+# Cleanup
+git restore zip/Boot/split/kernel
+git restore zip/Boot/split/ramdisk.cpio
+
+# Return to build directory
+cd $CUR_DIR
+
+printf "Flashable zip is ready at $OUT_DIR/Aroma.zip\n"
