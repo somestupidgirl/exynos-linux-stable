9252ce519835e03dce84d6dd91d54bb203f334a5
somestupidgirl <sunnevanattsol@gmail.com>
2025-01-24 14:58:08 +0000

    Build scripts: Major fixes and changes
    Build scripts should run straight out of the box now.
    
    To build the kernel run:
    
        $ ./build<device>.sh <stock/aosp> <ksu/noksu> <ext4/erofs>
    
    List of changes:
    
        - Add more coherent input arguments for KernelSU and EROFS to build scripts
    
        - Fix sed not working in some instances in variables.sh
    
        - Remove KernelSU from drivers/Kconfig and Makefile as it was causing non-KernelSU builds to fail
    
        - Add instructions to variables.sh for inserting KernelSU lines to drivers/Kconfig and Makefile when KernelSU is enabled
    
        - Make clean.sh clean up more KernelSU-related stuff
    
        - Automatically create output directories (../zip/kernel/... etc.) upon finishing builds
    
        - Make zip.sh create a flashable zip with Aroma installer after running build_kernel.sh

--- buildN9.sh
+++ buildN9.sh
@@ -4,6 +4,8 @@
 
 clean_temp
 
+printf "Building for device: crownlte\n"
+
 cp -vr $CUR_DIR/arch/arm64/configs/exynos9810_defconfig $CUR_DIR/arch/arm64/configs/exynos9810_temp_defconfig
 echo "" >> $CUR_DIR/arch/arm64/configs/exynos9810_temp_defconfig
 cat $CUR_DIR/arch/arm64/configs/exynos9810-crownlte_defconfig >> $CUR_DIR/arch/arm64/configs/exynos9810_temp_defconfig
@@ -12,28 +14,47 @@ if [ ! -d $CUR_DIR/toolchain/clang ] || [ ! -f $CUR_DIR/toolchain/clang/clang ];
   tc_setup
 fi
 
-if [ ! -z "$1" ]
-then  
+if [ ! -z "$1" ]; then  
   if [ "$1" == "stock" ]; then
     patch_stock
   elif [ "$1" == "aosp" ]; then
-	patch_aosp
+    patch_aosp
   fi
 fi
-if [ -z "$2" ]
-then  
-  patch_kernelsu
+
+if [ ! -z "$2" ]; then
+  if [ "$2" == "ksu" ]; then  
+    enable_kernelsu
+  elif [ "$2" == "noksu" ]; then
+    disable_kernelsu
+  fi
 fi
-if [ -z "$3" ]
-then  
-  dts_erofs
-else
-  dts_ext4
+
+if [ ! -z "$3" ]; then
+  if [ "$3" == "erofs" ]; then  
+    dts_erofs
+  elif [ "$3" == ext4 ]; then
+    dts_ext4
+  fi
 fi
 
 make exynos9810_temp_defconfig -j$(nproc --all)
 make -j$(nproc --all)
 
 printf $KERNEL_NAME
-cp -vr $CUR_DIR/arch/arm64/boot/Image $ZIP_DIR/$KERNEL_NAME/crownlte/zImage
-cp -vr $CUR_DIR/arch/arm64/boot/dtb.img $ZIP_DIR/$DTB_NAME/crownlte/dtb.img
+
+if [ ! -d $KERN_DIR/$KERNEL_NAME/crownlte ]; then
+  mkdir -p $KERN_DIR/$KERNEL_NAME/crownlte
+fi
+
+if [ ! -d $KERN_DIR/$DTB_NAME/crownlte ]; then
+  mkdir -p $KERN_DIR/$DTB_NAME/crownlte
+fi
+
+if [ ! -f $CUR_DIR/arch/arm64/boot/Image ] || [ ! -f $CUR_DIR/arch/arm64/boot/dtb.img ]; then
+  printf "Operation completed with errors!\n"
+else
+  cp -vr $CUR_DIR/arch/arm64/boot/Image $KERN_DIR/$KERNEL_NAME/crownlte/zImage
+  cp -vr $CUR_DIR/arch/arm64/boot/dtb.img $KERN_DIR/$DTB_NAME/crownlte/dtb.img
+  printf "Success!\n"
+fi
--- build_kernel.sh
+++ build_kernel.sh
@@ -1,22 +1,53 @@
 #!/bin/bash
 
+. variables.sh
+
+if [ ! -d $CUR_DIR/toolchain/clang ] || [ ! -f $CUR_DIR/toolchain/clang/clang ]; then
+  tc_setup
+fi
+
 . clean.sh all
-. buildN9.sh stock
-. buildN9.sh stock no
-. buildS9.sh stock
-. buildS9.sh stock no
-. buildS9+.sh stock
-. buildS9+.sh stock no
-. buildS9.sh default
-. buildS9.sh default no no
-. buildS9.sh aosp
-. buildS9.sh aosp no
-. buildN9.sh default
-. buildN9.sh default no no
-. buildN9.sh aosp
-. buildN9.sh aosp no
-. buildS9+.sh aosp
-. buildS9+.sh aosp no
-. buildS9+.sh default
-. buildS9+.sh default no no
+
+# Galaxy Note 9 (crownlte)
+. buildN9.sh stock ksu erofs
+. buildN9.sh stock ksu ext4
+. clean.sh
+. buildN9.sh stock noksu erofs
+. buildN9.sh stock noksu ext4
+. clean.sh
+. buildN9.sh aosp ksu erofs
+. buildN9.sh aosp ksu ext4
+. clean.sh
+. buildN9.sh aosp noksu erofs
+. buildN9.sh aosp noksu ext4
+. clean.sh
+
+# Galaxy S9 (starlte)
+. buildS9.sh stock ksu erofs
+. buildS9.sh stock ksu ext4
+. clean.sh
+. buildS9.sh stock noksu erofs
+. buildS9.sh stock noksu ext4
+. clean.sh
+. buildS9.sh aosp ksu ext4
+. buildS9.sh aosp ksu erofs
+. clean.sh
+. buildS9.sh aosp noksu erofs
+. buildS9.sh aosp noksu ext4
+. clean.sh
+
+# Galaxy S9+
+. buildS9+.sh stock ksu erofs
+. buildS9+.sh stock ksu ext4
+. clean.sh
+. buildS9+.sh stock noksu erofs
+. buildS9+.sh stock noksu ext4
+. clean.sh
+. buildS9+.sh aosp ksu erofs
+. buildS9+.sh aosp ksu ext4
+. clean.sh
+. buildS9+.sh aosp noksu erofs
+. buildS9+.sh aosp noksu ext4
+. clean.sh
+
 . zip.sh
