61b6773ad85488fb95cc7fb848a633c7e84742e9
Sunneva N. Mariu <sunnevanattsol@gmail.com>
2025-03-09 23:06:37 +0000

    net: wireless: realtek: rtl8812au: Fix after update
    Signed-off-by: Sunneva N. Mariu <sunnevanattsol@gmail.com>

--- drivers/net/wireless/realtek/rtl8812au/os_dep/linux/ioctl_cfg80211.c
+++ drivers/net/wireless/realtek/rtl8812au/os_dep/linux/ioctl_cfg80211.c
@@ -4769,12 +4769,12 @@ static int rtw_cfg80211_add_monitor_if(_adapter *padapter, char *name, struct ne
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 12 ,0))
 	mon_ndev->needs_free_netdev = true;
 #else
-	mon_ndev->destructor = rtw_ndev_destructor;
+	mon_ndev->priv_destructor = rtw_ndev_destructor;
 #endif
 #if (LINUX_VERSION_CODE > KERNEL_VERSION(4, 11, 8))
 	mon_ndev->priv_destructor = rtw_ndev_destructor;
 #else
-	mon_ndev->destructor = rtw_ndev_destructor;
+	mon_ndev->priv_destructor = rtw_ndev_destructor;
 #endif
 
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 29))
